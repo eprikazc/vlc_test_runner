f821effc04d5996446f800de51652ccd9d6e9d61
Eugene Prikazchikov <eprikazc@gmail.com>
2012-04-24 19:08:15 +0400

    Adding playback of test wmv file

--- vlc_test_runner.py
+++ vlc_test_runner.py
@@ -2,8 +2,10 @@ import urllib
 import os
 import subprocess
 
-vlc_archive_119 = "http://download.videolan.org/pub/videolan/vlc/1.1.9/vlc-1.1.9.tar.bz2"
-vlc_archive_200 = "http://download.videolan.org/pub/videolan/vlc/2.0.0/vlc-2.0.0.tar.xz"
+VLC_ARCHIVE_119 = "http://download.videolan.org/pub/videolan/vlc/1.1.9/vlc-1.1.9.tar.bz2"
+VLC_ARCHIVE_200 = "http://download.videolan.org/pub/videolan/vlc/2.0.0/vlc-2.0.0.tar.xz"
+
+TEST_MEDIA_FILE = "http://download.microsoft.com/download/6/8/f/68f212d7-f58d-4542-890d-65d7e790f2e0/The_Magic_of_Flight_720.exe"
 
 
 def run_shell_command(*args):
@@ -13,10 +15,12 @@ def run_shell_command(*args):
 
 
 def get_and_compile_vlc(vlc_archive_url):
-    os.chdir(os.path.expanduser("~"))
-    run_shell_command("wget", vlc_archive_url)
     file_name = vlc_archive_url.split('/')[-1]
     extension = file_name.split(".")[-1]
+    vlc_directory = file_name.split(".tar")[0]
+
+    os.chdir(os.path.expanduser("~"))
+    run_shell_command("wget", vlc_archive_url)
     if extension == "xz":
         tar_flags = "xfJ"
     elif extension == "bz2":
@@ -26,7 +30,6 @@ def get_and_compile_vlc(vlc_archive_url):
     print "Descompressing vlc source archive"
     run_shell_command("tar", tar_flags, file_name)
     run_shell_command("rm", file_name)
-    vlc_directory = file_name.split(".tar")[0]
     os.chdir(vlc_directory)
     run_shell_command("./configure", "--enable-x11", "--enable-xvideo", "--enable-sdl", "--enable-avcodec", "--enable-avformat",
  "--enable-swscale", "--enable-mad", "--enable-libdvbpsi", "--enable-a52", "--enable-libmpeg2", "--enable-dvdnav",
@@ -34,11 +37,27 @@ def get_and_compile_vlc(vlc_archive_url):
  "--enable-fribidi", "--enable-speex", "--enable-flac", "--enable-live555", "--with-live555-tree=/usr/lib/live",
  "--enable-caca", "--enable-skins", "--enable-skins2", "--enable-alsa", "--enable-qt4", "--enable-ncurses")
     run_shell_command("make")
-    run_shell_command("make", "clean")
+    return os.path.join(os.path.expanduser("~"), vlc_directory)
+
+
+def play_test_media_file(vlc_directory, test_media_file_name):
+    os.chdir(vlc_directory)
+    run_shell_command("./vlc", "--play-and-exit", test_media_file_name)
+
 
 def run():
-    run_shell_command("apt-get", "build-dep", "vlc", "-y")
-    get_and_compile_vlc(vlc_archive_119)
+    print "Downloading test media file"
+    os.chdir(os.path.expanduser("~"))
+    run_shell_command("wget", TEST_MEDIA_FILE)
+    test_media_file_name = TEST_MEDIA_FILE.split("/")[-1]
+    print "test media file downloaded. Extracting it."
+    run_shell_command("unzip", test_media_file_name)
+    test_media_file_name = test_media_file_name.replace(".exe", ".wmv")
+
+    print "Installing build dependencies for vlc"
+    run_shell_command("sudo", "apt-get", "build-dep", "vlc", "-y")
+    vlc_119_dir = get_and_compile_vlc(VLC_ARCHIVE_119)
+    play_test_media_file(vlc_119_dir, os.path.join(os.path.expanduser("~"), test_media_file_name))
 
 
 if __name__ == '__main__':
