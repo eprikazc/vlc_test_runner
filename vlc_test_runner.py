daed836f5d71e3956a23efa8451ef921e0ac4ced
Eugene Prikazchikov <eprikazc@gmail.com>
2012-04-25 18:30:36 +0400

    Added plot for CPU and memory stat

--- vlc_test_runner.py
+++ vlc_test_runner.py
@@ -1,6 +1,8 @@
 import urllib
 import os
 import subprocess
+import psutil
+from matplotlib import pyplot
 
 VLC_ARCHIVE_119 = "http://download.videolan.org/pub/videolan/vlc/1.1.9/vlc-1.1.9.tar.bz2"
 VLC_ARCHIVE_200 = "http://download.videolan.org/pub/videolan/vlc/2.0.0/vlc-2.0.0.tar.xz"
@@ -11,7 +13,7 @@ TEST_MEDIA_FILE = "http://download.microsoft.com/download/6/8/f/68f212d7-f58d-45
 def run_shell_command(*args):
     command = list(args)
     print "Running '%s'" %" ".join(command)
-    subprocess.call(command)
+    subprocess.check_call(command)
 
 
 def get_and_compile_vlc(vlc_archive_url):
@@ -41,8 +43,14 @@ def get_and_compile_vlc(vlc_archive_url):
 
 
 def play_test_media_file(vlc_directory, test_media_file_name):
-    os.chdir(vlc_directory)
-    run_shell_command("./vlc", "--play-and-exit", test_media_file_name)
+    cpu_data = []
+    memory_data = []
+
+    p = subprocess.Popen(["./vlc", "--play-and-exit", test_media_file_name], cwd=vlc_directory)
+    while p.poll() is None:
+        memory_data.append(psutil.phymem_usage().used)
+        cpu_data.append(psutil.cpu_percent(1))
+    return {"cpu_data": cpu_data, "memory_data": memory_data}
 
 
 def run():
@@ -57,8 +65,38 @@ def run():
     print "Installing build dependencies for vlc"
     run_shell_command("sudo", "apt-get", "build-dep", "vlc", "-y")
     vlc_119_dir = get_and_compile_vlc(VLC_ARCHIVE_119)
-    play_test_media_file(vlc_119_dir, os.path.join(os.path.expanduser("~"), test_media_file_name))
+    vlc_200_dir = get_and_compile_vlc(VLC_ARCHIVE_200)
+    play_test_media_file(vlc_1_dir, os.path.join(os.path.expanduser("~"), test_media_file_name))
+
+    vlc_119_stat = play_test_media_file(vlc_119_dir, '/home/eugene/The_Magic_of_Flight_720.wmv')
+    vlc_200_stat = play_test_media_file(vlc_200_dir, '/home/eugene/The_Magic_of_Flight_720.wmv')
+
+    pyplot.figure("CPU usage")
+    pyplot.plot(vlc_data_1["cpu_data"], label="vlc_1.x")
+    pyplot.plot(vlc_data_2["cpu_data"], label="vlc_2.0")
+    pyplot.legend()
+    pyplot.figure("Memory usage")
+    pyplot.plot(vlc_data_1["memory_data"], label="vlc_1.x")
+    pyplot.plot(vlc_data_2["memory_data"], label="vlc_2.0")
+    pyplot.legend()
+    pyplot.show()
+
 
 
 if __name__ == '__main__':
-    run()
+#    run()
+    vlc_data_1 = play_test_media_file('/home/eugene/vlc-1.1.9/', '/home/eugene/The_Magic_of_Flight_720.wmv')
+    vlc_data_2 = play_test_media_file('/home/eugene/vlc-2.0.0/', '/home/eugene/The_Magic_of_Flight_720.wmv')
+
+    pyplot.figure("CPU usage")
+    pyplot.plot(vlc_data_1["cpu_data"], label="vlc_1.x")
+    pyplot.plot(vlc_data_2["cpu_data"], label="vlc_2.0")
+    pyplot.legend()
+    pyplot.figure("Memory usage")
+    pyplot.plot(vlc_data_1["memory_data"], label="vlc_1.x")
+    pyplot.plot(vlc_data_2["memory_data"], label="vlc_2.0")
+    pyplot.legend()
+    print len(vlc_data_1["memory_data"])
+    print len(vlc_data_2["memory_data"])
+
+    pyplot.show()
